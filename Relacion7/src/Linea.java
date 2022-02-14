a92219145cb7c0c04dc68a0fafca21fd5663ae0a
chisela14 <chisela14@gmail.com>
2022-02-14 12:23:50 +0100

    14-2

---
+++ Relacion7/src/Linea.java
@@ -0,0 +1,17 @@
+
+public class Linea {
+	
+	//Atributos
+	private String puntoA, puntoB;
+	private int x,y;
+	
+	//Constructores
+	public Linea () {
+		this.puntoA = 0.0;
+		this.puntoB = 0.0;
+	}
+	public Linea (double punto, double punto2) {
+		this.puntoA = punto;
+		this.puntoB = punto2;
+	}
+}
