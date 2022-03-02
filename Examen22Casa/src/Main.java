9150e2ef881635fba7f8b8951ff39142f5524ac8
chisela14 <chisela14@gmail.com>
2022-03-02 19:45:29 +0100

    examen casi acabado

--- Examen22Casa/src/Main.java
+++ Examen22Casa/src/Main.java
@@ -7,17 +7,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Plato> listaPlatos = new ArrayList<Plato> ();
-		int opcion;
+		char opcion;
 		
 		do {
 			mostrarMenu();
 			opcion = teclado.nextLine().charAt(0);
-			if (opcion == 1) {
+			try {
+				//cambiar a swtich
+				if (opcion == '1') {
+					darAlta (listaPlatos);
+				}else if (opcion == '2'){
+					modificarPrecio (listaPlatos);
+				}else if (opcion == '5') {
+					mostrarPlatos (listaPlatos);
+				}else if (opcion=='3') {
+					asignarVino (listaPlatos);
+				}
 				
-				darAlta (listaPlatos);
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+				continue;
 			}
 			
-		}while (opcion !=6);
+		}while (opcion != '6');
 		
 	}
 	
@@ -32,21 +44,61 @@ public class Main {
 		
 	}
 	
-	public static void darAlta (ArrayList<Plato> listaPlatos) {
+	public static void darAlta (ArrayList<Plato> listaPlatos) throws Exception {
 		System.out.println("Introduce el nombre del plato: ");
 		String nombre = teclado.nextLine();
-		for (int i=0; i<listaPlatos.size(); i++) {
-			Plato plato = listaPlatos.get(i);
-			if (plato.getNombre() == nombre) {
-				System.out.println("No se puede añadir el plato porque ya existe");
-				//break;
-			}
-		}
+		validarPlato (listaPlatos, nombre);
 		System.out.println("Introduce el precio: ");
 		double precio = Double.parseDouble(teclado.nextLine());
+		if (precio <= 0) {
+			throw new Exception ("El precio no es valido");
+		}
 		Plato p1 = new Plato (nombre, precio);
 		listaPlatos.add (p1);
+		System.out.println(listaPlatos);
 		
+	}
+	public static void validarPlato (ArrayList<Plato> listaPlatos, String nombre) throws Exception {
+		for (int i=0; i<listaPlatos.size(); i++) {
+			Plato plato = listaPlatos.get(i);
+			if (plato.getNombre().equals(nombre)) {
+				throw new Exception ("No se puede añadir el plato porque ya existe"); 
+			}
+		}
+	}
+	public static void modificarPrecio (ArrayList<Plato> listaPlatos)throws Exception{
+		System.out.println("Introduce el nombre del plato: ");
+		String nombre = teclado.nextLine();
+		Plato plato =  obtenerPlato (listaPlatos, nombre);
+		System.out.println("Introduce el precio: ");
+		double precio = Double.parseDouble(teclado.nextLine());
+		plato.setPrecio (precio);
+		
+	}
+	public static Plato obtenerPlato (ArrayList<Plato> listaPlatos, String nombre) throws Exception {
+		for (int i=0; i<listaPlatos.size(); i++) {
+			Plato plato = listaPlatos.get(i);
+			if (plato.getNombre().equals(nombre)) {
+				return plato;
+			}
+		}
+		throw new Exception ("No existe ningún plato con ese nombre");
+	}
+	public static void asignarVino (ArrayList<Plato> listaPlatos) throws Exception {
+		System.out.println("Introduce el nombre del plato: ");
+		String nombrePlato = teclado.nextLine();
+		Plato plato = obtenerPlato (listaPlatos, nombrePlato);
+		System.out.println("Introduce el nombre del vino: ");
+		String nombreVino = teclado.nextLine();
+		System.out.println("Introduce la graduacion del vino: ");
+		double graduacion = Double.parseDouble(teclado.nextLine());
+		Vino v1 = new Vino (nombreVino, graduacion);
+		plato.setVinoRecomendado(v1);
+	}
+	
+	
+	public static void mostrarPlatos (ArrayList<Plato> listaPlatos) {
+		System.out.println(listaPlatos);
 	}
 
 }
